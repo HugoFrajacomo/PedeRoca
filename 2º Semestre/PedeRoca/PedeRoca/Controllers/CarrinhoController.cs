c47b7a406defc7604f8cb2d0444889add12a9fad
Hugo Frajacomo <125396485+HugoFrajacomo@users.noreply.github.com>
2023-12-06 08:05:12 -0300

    Backup

---
+++ 2º Semestre/PedeRoca/PedeRoca/Controllers/CarrinhoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PedeRoca.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        // 1) Criar um repositório
+        private readonly Repositories.ADO.SQLServer.CarrinhoDAO repository;
+
+        // 2) Criar o construtor para o repositório
+        public CarrinhoController(IConfiguration configuration) //Passar uma configuração
+        {
+            //trás a chave default connection para conectar no banco
+            this.repository = new Repositories.ADO.SQLServer.CarrinhoDAO(configuration.GetConnectionString(Configurations.AppSettings.getKeyConnectionString()));
+        }
+
+        //----------------------------- Listar Por ID - ADM --------------------------
+        #region "Listar Carrinho por ID"
+        // Post: ProdutoController - Details por ID //Imformaçao por ID
+        [HttpGet]
+        public IActionResult CarrinhoDeCompras(int idUsuario)
+        {
+            if (idUsuario != 0)
+            {
+                return View(this.repository.Carrinho(idUsuario));
+            }
+            else
+            {
+                return RedirectToAction("CarrioVazio");
+            }
+        }
+        #endregion
+
+        //----------------------------- Listar Por ID - ADM --------------------------
+        #region "CarrinhoVazio"
+
+        public IActionResult CarrioVazio()
+        {
+            return View();
+        }
+
+        #endregion
+
+        //----------------------------- Listar items do Carrinho ---------------------
+        #region "Listar Produtos"
+        // GET: ProdutoController - ListarTodosProdutos
+        [HttpGet]
+        public IActionResult Carrinho(int id)
+        {
+            return View(this.repository.ListarTodosProdutos(id));
+        }
+        #endregion
+    }
+}
--- 2º Semestre/PedeRoca/PedeRoca/Repositories/ADO/SQLServer/CarrinhoDAO.cs
+++ 2º Semestre/PedeRoca/PedeRoca/Repositories/ADO/SQLServer/CarrinhoDAO.cs
@@ -21,13 +21,15 @@ namespace PedeRoca.Repositories.ADO.SQLServer
         public Carrinho Carrinho(int idUsuario)
         {
             Carrinho carrinho = new Carrinho();
+            List<ItemDoCarrinho> itemDoCarrinho = new List<ItemDoCarrinho>();
+
             using (SqlConnection connection = new SqlConnection(this.connectionString))
             {
                 connection.Open();
                 using (SqlCommand command = new SqlCommand())
                 {
                     command.Connection = connection;
-                    command.CommandText = "SELECT u.id_usuario, p.id_produto, cc.id_carrinhoCompra, ic.id_itemCarrinho, p.nome FROM tb_carrinhoCompras AS CC INNER JOIN tb_usuarios AS U ON (CC.id_usuario = U.id_usuario) INNER JOIN tb_itensCarrinho AS IC ON (IC.id_carrinhoCompra = CC.id_carrinhoCompra) INNER JOIN tb_produtos AS P ON (IC.id_produto = P.id_produto) where U.id_usuario=@id;";
+                    command.CommandText = "SELECT u.id_usuario, p.id_produto, cc.id_carrinhoCompra, ic.id_itemCarrinho, IC.quantidade, p.nome, p.preco_unitario FROM tb_carrinhoCompras AS CC INNER JOIN tb_usuarios AS U ON (CC.id_usuario = U.id_usuario) INNER JOIN tb_itensCarrinho AS IC ON (IC.id_carrinhoCompra = CC.id_carrinhoCompra) INNER JOIN tb_produtos AS P ON (IC.id_produto = P.id_produto) where U.id_usuario=@id;";
                     command.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.Int)).Value = idUsuario;
 
                     //Onde será retornada a informação da consulta do banco
@@ -37,7 +39,19 @@ namespace PedeRoca.Repositories.ADO.SQLServer
                     if (dr.Read())
                     {
                         carrinho.Id_Carrinho = (int)dr["id_carrinhoCompra"];
-                        carrinho.pessoa.Id_usuario = (int)dr["id_usuario"];
+                        carrinho.pessoa.Id_usuario = (int)dr["u.id_usuario"];
+
+                        ItemDoCarrinho itensDoCarrinho = new ItemDoCarrinho();
+                        itensDoCarrinho.Id_itemCarrinho = (int)dr["id_carrinhoCompra"];
+                        itensDoCarrinho.Quantidade = (int)dr["IC.quantidade"];
+
+                        Produto produto = new Produto();
+                        produto.Id_produtos = (int)dr["p.id_produto"];
+                        produto.Nome = (string)dr["p.nome"];
+                        produto.PrecoUnitario = (decimal)dr["p.preco_unitario"];
+
+                        itensDoCarrinho.Produto = produto;
+                        carrinho.Item.Add(itensDoCarrinho);
                     }
                 }
             }
@@ -68,7 +82,42 @@ namespace PedeRoca.Repositories.ADO.SQLServer
         #endregion
 
         //------------- Item do Carrinho ----------------------------
+        #region "Listar todos os Produtos"
+        public List<ItemDoCarrinho> ListarTodosProdutos(int id)
+        {
+            List<ItemDoCarrinho> itemDoCarrinhos = new List<ItemDoCarrinho>();
 
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                //Abrir a conexão do bando de dados: PedeRoça
+                connection.Open();
+
+                // Instrodução do comando SQL
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "SELECT id_itemCarrinho, id_produto, c.id_carrinhoCompra, quantidade FROM tb_itensCarrinho AS C INNER JOIN tb_compras AS COMP ON (COMP.id_carrinhoCompra = C.id_carrinhoCompra) INNER JOIN tb_usuarios AS U ON (COMP.id_usuario = U.id_usuario) WHERE U.id_usuario=@id ";
+                    command.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.Int)).Value = id;
+
+                    //Onde será retornada a informação da consulta do banco
+                    SqlDataReader dr = command.ExecuteReader(); //objeto de fluxo de dados
+
+                    //Criando um objeto para cada leitura do banco
+                    while (dr.Read())
+                    {
+                        ItemDoCarrinho itemDoCarrinho = new ItemDoCarrinho();
+                        itemDoCarrinho.Id_itemCarrinho = (int)dr["id_itemCarrinho"];
+                        itemDoCarrinho.Produto.Id_produtos = (int)dr["id_produto"];
+                        itemDoCarrinho.Quantidade = (int)dr["quantidade"];
+
+
+                        itemDoCarrinhos.Add(itemDoCarrinho);
+                    }
+                }
+            }
+            return itemDoCarrinhos;
+        }
+        #endregion
 
     }
 }
